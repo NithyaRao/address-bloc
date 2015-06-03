4927619f61ec042f99879058ec000d8556177937
Nithya Rao <bumblebeenr@gmail.com>
2015-06-03 14:40:46 -0500

    Checkpoint23 before assignment

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
 require_relative "entry.rb"
+require "csv"
 
 class AddressBook
  attr_accessor :entries
@@ -21,5 +22,15 @@ class AddressBook
    
    def import_from_csv(file_name)
      # Implementation goes here
+      csv_text = File.read(file_name)
+      csv = CSV.parse(csv_text, headers: true)
+ # #8
+      csv.each do |row|
+       row_hash = row.to_hash
+       puts row_hash
+       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+      end
+ 
+     return csv.count
    end
 end
--- specs/address_book_specs.rb
+++ specs/address_book_specs.rb
@@ -55,29 +55,34 @@ context ".import_from_csv" do
        entry_one = book.entries[0]
        check_entry(entry_one, "Bill", "555-555-5555", "bill@blocmail.com")      
       end
+
       it "checks the details of the second entry" do
        book.import_from_csv("entries.csv")
        # Check the first entry
-       entry_one = book.entries[1]
+       entry_two = book.entries[1]
       check_entry(entry_two, "Bob", "555-555-5555", "bob@blocmail.com")
       end
+
       it "checks the details of the third entry" do
        book.import_from_csv("entries.csv")
        # Check the first entry
-       entry_one = book.entries[2]
+       entry_three = book.entries[2]
        check_entry(entry_three, "Joe", "555-555-5555", "joe@blocmail.com")
       end
+
       it "checks the details of the fourth entry" do
        book.import_from_csv("entries.csv")
        # Check the first entry
-       entry_one = book.entries[3]
-        check_entry(entry_three, "Sally", "555-555-5555", "sally@blocmail.com")
+       entry_four = book.entries[3]
+        check_entry(entry_four, "Sally", "555-555-5555", "sally@blocmail.com")
       end 
+
       it "checks the details of the fifth entry" do
        book.import_from_csv("entries.csv")
        # Check the first entry
-       entry_one = book.entries[4]
-        check_entry(entry_three, "Sussie", "555-555-5555", "sussie@blocmail.com")
+       entry_five = book.entries[4]
+        check_entry(entry_five, "Sussie", "555-555-5555", "sussie@blocmail.com")
       end 
+
    end 
 end
